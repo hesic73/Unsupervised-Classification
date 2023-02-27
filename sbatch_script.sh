752c5b7c7e8374b7150fb1091f7cfe47db72b083
Tianfang Zhao <zhaotianfang2010@163.com>
2023-02-27 22:08:22 +0800

    add simple tensorboard logging

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -14,5 +14,7 @@
 #SBATCH --mail-user hesicheng2001@163.com  
 
 cd /Share/UserHome/tzhao/2023/sicheng/GraduationDesign/Unsupervised-Classification
+echo 开始跑了喵
 python simclr.py --config_env custom/configs/env.yml \
 --config_exp custom/configs/exp_env.yml
+echo 跑完了喵
--- simclr.py
+++ simclr.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import torch
 import numpy as np
+import time
 
 from utils.config import create_config
 from utils.common_config import get_criterion, get_model, get_train_dataset,\
@@ -18,6 +19,8 @@ from utils.memory import MemoryBank
 from utils.train_utils import simclr_train
 from utils.utils import fill_memory_bank
 from termcolor import colored
+
+from torch.utils.tensorboard import SummaryWriter
 
 # Parser
 parser = argparse.ArgumentParser(description='SimCLR')
@@ -32,7 +35,7 @@ def main():
     # Retrieve config file
     p = create_config(args.config_env, args.config_exp)
     print(colored(p, 'red'))
-    
+
     # Model
     print(colored('Retrieve model', 'blue'))
     model = get_model(p)
@@ -40,11 +43,11 @@ def main():
     print('Model parameters: {:.2f}M'.format(sum(p.numel() for p in model.parameters()) / 1e6))
     print(model)
     model = model.cuda()
-   
+
     # CUDNN
-    print(colored('Set CuDNN benchmark', 'blue')) 
+    print(colored('Set CuDNN benchmark', 'blue'))
     torch.backends.cudnn.benchmark = True
-    
+
     # Dataset
     print(colored('Retrieve dataset', 'blue'))
     train_transforms = get_train_transformations(p)
@@ -53,16 +56,16 @@ def main():
     print('Validation transforms:', val_transforms)
     train_dataset = get_train_dataset(p, train_transforms, to_augmented_dataset=True,
                                         split='train+unlabeled') # Split is for stl-10
-    val_dataset = get_val_dataset(p, val_transforms) 
+    val_dataset = get_val_dataset(p, val_transforms)
     train_dataloader = get_train_dataloader(p, train_dataset)
     val_dataloader = get_val_dataloader(p, val_dataset)
     print('Dataset contains {}/{} train/val samples'.format(len(train_dataset), len(val_dataset)))
-    
+
     # Memory Bank
     print(colored('Build MemoryBank', 'blue'))
     base_dataset = get_train_dataset(p, val_transforms, split='train') # Dataset w/o augs for knn eval
-    base_dataloader = get_val_dataloader(p, base_dataset) 
-    memory_bank_base = MemoryBank(len(base_dataset), 
+    base_dataloader = get_val_dataloader(p, base_dataset)
+    memory_bank_base = MemoryBank(len(base_dataset),
                                 p['model_kwargs']['features_dim'],
                                 p['num_classes'], p['criterion_kwargs']['temperature'])
     memory_bank_base.cuda()
@@ -81,7 +84,7 @@ def main():
     print(colored('Retrieve optimizer', 'blue'))
     optimizer = get_optimizer(p, model)
     print(optimizer)
- 
+
     # Checkpoint
     if os.path.exists(p['pretext_checkpoint']):
         print(colored('Restart from checkpoint {}'.format(p['pretext_checkpoint']), 'blue'))
@@ -95,9 +98,13 @@ def main():
         print(colored('No checkpoint file at {}'.format(p['pretext_checkpoint']), 'blue'))
         start_epoch = 0
         model = model.cuda()
-    
+
     # Training
     print(colored('Starting main loop', 'blue'))
+
+    writer = SummaryWriter(
+        log_dir=os.path.join("logs/", time.strftime("%Y%m%d-%H%M%S")))
+
     for epoch in range(start_epoch, p['epochs']):
         print(colored('Epoch %d/%d' %(epoch, p['epochs']), 'yellow'))
         print(colored('-'*15, 'yellow'))
@@ -105,7 +112,7 @@ def main():
         # Adjust lr
         lr = adjust_learning_rate(p, optimizer, epoch)
         print('Adjusted learning rate to {:.5f}'.format(lr))
-        
+
         # Train
         print('Train ...')
         simclr_train(train_dataloader, model, criterion, optimizer, epoch)
@@ -117,37 +124,37 @@ def main():
         # Evaluate (To monitor progress - Not for validation)
         print('Evaluate ...')
         top1 = contrastive_evaluate(val_dataloader, model, memory_bank_base)
-        print('Result of kNN evaluation is %.2f' %(top1)) 
-        
+        print('Result of kNN evaluation is %.2f' %(top1))
+        writer.add_scalar('knn_evaluation', top1, epoch)
         # Checkpoint
         print('Checkpoint ...')
-        torch.save({'optimizer': optimizer.state_dict(), 'model': model.state_dict(), 
+        torch.save({'optimizer': optimizer.state_dict(), 'model': model.state_dict(),
                     'epoch': epoch + 1}, p['pretext_checkpoint'])
 
     # Save final model
     torch.save(model.state_dict(), p['pretext_model'])
 
-    # Mine the topk nearest neighbors at the very end (Train) 
+    # Mine the topk nearest neighbors at the very end (Train)
     # These will be served as input to the SCAN loss.
     print(colored('Fill memory bank for mining the nearest neighbors (train) ...', 'blue'))
     fill_memory_bank(base_dataloader, model, memory_bank_base)
     topk = 20
-    print('Mine the nearest neighbors (Top-%d)' %(topk)) 
+    print('Mine the nearest neighbors (Top-%d)' %(topk))
     indices, acc = memory_bank_base.mine_nearest_neighbors(topk)
     print('Accuracy of top-%d nearest neighbors on train set is %.2f' %(topk, 100*acc))
-    np.save(p['topk_neighbors_train_path'], indices)   
+    np.save(p['topk_neighbors_train_path'], indices)
 
-   
+
     # Mine the topk nearest neighbors at the very end (Val)
     # These will be used for validation.
     print(colored('Fill memory bank for mining the nearest neighbors (val) ...', 'blue'))
     fill_memory_bank(val_dataloader, model, memory_bank_val)
     topk = 5
-    print('Mine the nearest neighbors (Top-%d)' %(topk)) 
+    print('Mine the nearest neighbors (Top-%d)' %(topk))
     indices, acc = memory_bank_val.mine_nearest_neighbors(topk)
     print('Accuracy of top-%d nearest neighbors on val set is %.2f' %(topk, 100*acc))
-    np.save(p['topk_neighbors_val_path'], indices)   
+    np.save(p['topk_neighbors_val_path'], indices)
 
- 
+
 if __name__ == '__main__':
     main()
